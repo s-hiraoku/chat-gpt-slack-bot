d69fc02d88c66c0af8a41023005307a8d1d6ed7f
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-10-02 09:26:46 +0900

    create dev template.

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,36 @@
-import { App } from "@slack/bolt";
-import { load } from "ts-dotenv";
+import { App } from '@slack/bolt';
+import { load } from 'ts-dotenv';
+import { Env } from './types';
 
-const env = load({
-  SLACK_BOT_TOKEN: String,
-  SLACK_SIGNING_SECRET: String,
-  PORT: Number,
-});
+let env: Partial<Env>;
+try {
+  env = load({
+    SLACK_BOT_TOKEN: String,
+    SLACK_SIGNING_SECRET: String,
+    PORT: Number,
+  });
+} catch (err) {
+  console.error('Failed to load environment variables', err);
+  process.exit(1);
+}
+
+if (!env.SLACK_BOT_TOKEN || !env.SLACK_SIGNING_SECRET) {
+  console.error('SLACK_BOT_TOKEN and/or SLACK_SIGNING_SECRET are not set in the environment variables');
+  process.exit(1);
+}
 
 const app = new App({
   token: env.SLACK_BOT_TOKEN,
   signingSecret: env.SLACK_SIGNING_SECRET,
 });
 
-app.message("", async ({ message, say }) => {
-  if (!message.subtype) {
-    await say(`Hello, <@${message.user}>. You said: ${message.text}`);
-  }
+app.event('app_mention', async ({ event, say }) => {
+  console.log('Mention event', event);
+  await say(`Hello, <@${event.user}>. You mentioned me with: ${event.text}`);
 });
 
 (async () => {
   await app.start(process.env.PORT || 3000);
 
-  console.log("⚡️ Bolt app is running!");
+  console.log('⚡️ Bolt app is running!');
 })();
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type Env = {
+  SLACK_BOT_TOKEN: string;
+  SLACK_SIGNING_SECRET: string;
+  PORT: number;
+};
