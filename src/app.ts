522534796999bd5f478d2b1e6001e93f3c201b01
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-10-08 18:20:17 +0900

    refactoring app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
-import { App } from '@slack/bolt';
+import { App, SayFn } from '@slack/bolt';
 import { OpenAIMessages } from './types';
 import { covertSlackMessageToOpenAIMessage, getSlackBotId } from './slack';
-import { EMPTY_STRING, getEnv, sanitizeText } from './utils';
+import { getEnv, sanitizeText } from './utils';
 import { initOpenAI, sendMessage } from './openai';
 import { REVIEW_REQUEST_MESSAGE, SLACK_EMPTY_MESSAGE_REPLY } from './messages/slack';
 
-let slackBotId: string | undefined;
 const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, OPENAI_API_KEY, OPENAI_MODEL } = getEnv();
+
+if (!SLACK_BOT_TOKEN || !OPENAI_API_KEY) {
+  console.error(' SLACK_BOT_TOKEN or OPENAI_API_KEY are not set in the environment variables');
+  process.exit(1);
+}
+
+let slackBotId: string | undefined;
+
+const replyWithEventTS = async (say: SayFn, text: string, eventTS: string) => {
+  await say({
+    text,
+    thread_ts: eventTS,
+  });
+};
 
 const app = new App({
   token: SLACK_BOT_TOKEN,
@@ -14,23 +27,18 @@ const app = new App({
 });
 
 app.event('app_mention', async ({ event, client, say }) => {
+  const { text: currentText, ts: eventTS, user } = event;
   const threadTs = event.thread_ts ? event.thread_ts : event.ts;
 
-  const currentMessage = sanitizeText(event.text);
+  const currentMessage = sanitizeText(currentText);
   console.log('Current message:', currentMessage);
 
-  if (currentMessage === EMPTY_STRING) {
-    await say({
-      text: SLACK_EMPTY_MESSAGE_REPLY,
-      thread_ts: event.ts,
-    });
+  if (!currentMessage) {
+    await replyWithEventTS(say, SLACK_EMPTY_MESSAGE_REPLY, eventTS);
     return;
   }
   if (currentMessage === REVIEW_REQUEST_MESSAGE) {
-    await say({
-      text: 'Please send the code to be reviewed.',
-      thread_ts: event.ts,
-    });
+    await replyWithEventTS(say, 'Please send the code to be reviewed.', eventTS);
     return;
   }
 
@@ -53,7 +61,7 @@ app.event('app_mention', async ({ event, client, say }) => {
   } catch (err) {
     console.error(err);
     await say({
-      text: `<@${event.user}>\n A defect has occurred. Please contact the developer.\n\n${err}`,
+      text: `<@${user}>\n A defect has occurred. Please contact the developer.\n\n${err}`,
       thread_ts: threadTs,
     });
   }
@@ -62,14 +70,6 @@ app.event('app_mention', async ({ event, client, say }) => {
 (async () => {
   await app.start(process.env.PORT || 3000);
 
-  if (!SLACK_BOT_TOKEN) {
-    console.error(' SLACK_BOT_TOKEN are not set in the environment variables');
-    process.exit(1);
-  }
-  if (!OPENAI_API_KEY) {
-    console.error(' OPENAI_API_KEY are not set in the environment variables');
-    process.exit(1);
-  }
   slackBotId = await getSlackBotId(app, SLACK_BOT_TOKEN);
 
   await initOpenAI(OPENAI_API_KEY, OPENAI_MODEL);
