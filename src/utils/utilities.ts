b0aa9c1829d0fcf497d18364e33455e7105e50a3
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-10-09 09:26:37 +0900

    add file utils.

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,7 +1,13 @@
+import { SlackFiles } from '../types';
+
 export const SLACK_MENTION_REGEXP = /<@[A-Za-z0-9]+>\n/;
 export const NEWLINE_REGEXP = /\n/g;
 export const EMPTY_STRING = '';
 
 export const sanitizeText = (text: string) => {
   return text.replace(SLACK_MENTION_REGEXP, '').replace(NEWLINE_REGEXP, '');
+};
+
+export const validateFiles = (files: SlackFiles) => {
+  return files.length > 0 && files.length <= 1 && files[0].filetype === 'zip';
 };
