a89e699896f63801de661bf912998c593d29332f
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-10-15 18:04:41 +0900

    refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import { App } from '@slack/bolt';
-import { getSlackBotId } from './slack';
-import { sanitizeText } from './utils';
+import { getSlackBotId, sendMessageToSlack } from './slack';
 import { initOpenAI } from './openai';
 import { REVIEW_REQUEST_MESSAGE, SLACK_EMPTY_MESSAGE_REPLY } from './messages';
-import { getEnvVariables, processReviewCode, processThreadMessages, replyWithEventTS } from './helpers';
+import { getEnvVariables, processReviewCode, getOpenAIResponseAndReplyToThread } from './helpers';
 
 let slackBotId: string | undefined;
 
-const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, OPENAI_API_KEY, OPENAI_MODEL } = getEnvVariables();
+const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, PORT, OPENAI_API_KEY, OPENAI_MODEL } = getEnvVariables();
 
 const app = new App({
   token: SLACK_BOT_TOKEN,
@@ -18,23 +17,23 @@ app.event('app_mention', async ({ event, client, say }) => {
   const { text: currentText, ts: eventTS, user } = event;
   const threadTs = event.thread_ts ? event.thread_ts : event.ts;
 
-  const currentMessage = sanitizeText(currentText);
-  console.log('Current message:', currentMessage);
+  console.log('Current message:', currentText);
 
-  if (!currentMessage) {
-    await replyWithEventTS(say, SLACK_EMPTY_MESSAGE_REPLY, eventTS);
+  if (!currentText) {
+    await sendMessageToSlack(say, SLACK_EMPTY_MESSAGE_REPLY, eventTS);
     return;
   }
-  if (currentMessage === REVIEW_REQUEST_MESSAGE) {
+
+  if (currentText === REVIEW_REQUEST_MESSAGE) {
     processReviewCode(client, event, eventTS, SLACK_BOT_TOKEN, slackBotId, say);
     return;
   }
 
-  processThreadMessages(client, event.channel, threadTs, user, slackBotId, say);
+  getOpenAIResponseAndReplyToThread(client, event.channel, threadTs, user, slackBotId, say);
 });
 
-(async () => {
-  await app.start(process.env.PORT || 3000);
+(async (): Promise<void> => {
+  await app.start(PORT || 3000);
 
   slackBotId = await getSlackBotId(app, SLACK_BOT_TOKEN);
 
